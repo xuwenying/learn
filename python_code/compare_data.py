fc76116212193455480db04539490c6dad2c0e80
Xuwenying <87738179@qq.com>
2020-05-07 10:49:07 +0800

    Update compare_data.py

--- python_code/compare_data.py
+++ python_code/compare_data.py
@@ -4,7 +4,7 @@ imgdata=img.reshape(3,1280,1920)
 
 conv2_data = np.fromfile(file_path+'0003_00024_00320x00192.y', dtype=np.int8)
 conv2_data = conv2_data.reshape(24, 192, 320)
-
+conv2_data.tofile("conv2_data.bin")
 
 filessss='RPNGEN.txt'
 b = np.loadtxt(filessss, dtype=np.int8)
