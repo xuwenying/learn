7debc1c87816a071fd21946ea1997fd8f2395064
Xuwenying <87738179@qq.com>
2020-05-07 10:46:02 +0800

    Create compare_data.py

---
+++ python_code/compare_data.py
@@ -0,0 +1,11 @@
+image_name = 'D:/TDA4X_Learning/Team_work/000.jpeg'    #bgr
+img = cv2.imread(image_name)
+imgdata=img.reshape(3,1280,1920)
+
+conv2_data = np.fromfile(file_path+'0003_00024_00320x00192.y', dtype=np.int8)
+conv2_data = conv2_data.reshape(24, 192, 320)
+
+
+filessss='RPNGEN.txt'
+b = np.loadtxt(filessss, dtype=np.int8)
+print(b.mean())
